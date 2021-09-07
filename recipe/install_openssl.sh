e7573da5b946f73c9323b100c2519a232f4c7f7b
H. Vetinari <h.vetinari@gmx.com>
2021-09-07 19:00:16 +0200

    reflect new version (and version policy)

--- recipe/install_openssl.sh
+++ recipe/install_openssl.sh
@@ -5,8 +5,8 @@ make install_sw install_ssldirs
 
 # https://github.com/ContinuumIO/anaconda-issues/issues/6424
 if [[ ${HOST} =~ .*linux.* ]]; then
-  if execstack -q "${PREFIX}"/lib/libcrypto.so.1.1 | grep -e '^X '; then
-    echo "Error, executable stack found in libcrypto.so.1.1"
+  if execstack -q "${PREFIX}"/lib/libcrypto.so.3.0 | grep -e '^X '; then
+    echo "Error, executable stack found in libcrypto.so.3.0"
     exit 1
   fi
 fi
