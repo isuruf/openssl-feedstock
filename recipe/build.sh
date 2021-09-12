3cd4dbbc461a2b9cc5243897fe8c8e00ac430e8f
H. Vetinari <h.vetinari@gmx.com>
2021-09-12 14:08:03 +0200

    simpler solution: unset HTTP_PROXY

--- recipe/build.sh
+++ recipe/build.sh
@@ -38,20 +38,9 @@ CC=${CC}" ${CPPFLAGS} ${CFLAGS}" \
 
 make -j${CPU_COUNT}
 
-# replace 127.0.0.1 with whatever hostname is on azure CI so that cmp_http tests pass;
+# unset HTTP_PROXY variable that otherwise causes cmp_http tests to fail;
 # see https://github.com/openssl/openssl/issues/16546
-if [[ "$target_platform" = "linux-"* ]]; then
-  HOST_IP=$(hostname -i)
-else
-  # WIP: to grep
-  ifconfig
-fi
-if [[ -n ${HOST_IP+x} ]]; then
-  echo "Replacing 127.0.0.1 with $HOST_IP"
-  sed -i.bak "s/127.0.0.1/$HOST_IP/g" test/recipes/80-test_cmp_http.t test/recipes/80-test_cmp_http_data/Mock/test.cnf
-else
-  echo "Not applying workaround"
-fi
+unset HTTP_PROXY
 
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
   echo "Running tests"
